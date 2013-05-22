63be032ad005ef291681805672e9f5c2c9f63023
Platonn <platonn@platonn.com>
2013-05-22 19:11:47 +0200

    Dodano Sedziego i Sedziego-sumienie. #21
    Napisalem calkiem porzadnie oprogramowanego i udokumentowanego Sedziego i
    dziedziczacego z niego Sedziego-sumienie.

---
+++ src/com/fishkey/RefereeConscience.java
@@ -0,0 +1,26 @@
+package com.fishkey;
+
+/**
+ * Sedzia sumienie ocenia "naiwnie" poprawnosc odpowiedi,
+ * bazujac na informacji, czy odpowiadajacy znal poprawna odpowiedz
+ * @author Platon
+ *
+ */
+public class RefereeConscience extends Referee {
+	public RefereeConscience(Flashcard f) {
+		super(f);
+	}
+	
+	/**
+	 * zwraca info, czy odpowiadajacy znal poprawna odpowiedz
+	 * <p>
+	 * ocenia poprawnosc odpowiedzi, bazujac "ufnie" na informacji,
+	 * czy odpowiadajacy znal poprawna odpowiedz
+	 * @param knew	informacja, czy odpowiadajacy znal poprawna odpowiedz
+	 * @return 		true, jesli odpowiadajacy znal poprawna odpowiedz,
+	 * 				false w przeciwnym przypadku
+	 */
+	public boolean adjudicate(boolean knew){
+		return knew;
+	}
+}
