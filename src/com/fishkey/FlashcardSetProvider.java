1506a02e694fe8f737c9a722cb6721bd48917d38
Platonn <platonn@platonn.com>
2013-05-22 19:13:56 +0200

    Puste klasy: MenadzerPytan, Stats, flashcProvider
    Napisalem puste szablony klas: #20
    	new file:   src/com/fishkey/QuizManager.java
    	new file:   src/com/fishkey/Statistics.java
    	new file:   src/com/fishkey/FlashcardSetProvider.java

---
+++ src/com/fishkey/FlashcardSetProvider.java
@@ -0,0 +1,11 @@
+package com.fishkey;
+
+/**
+ * Dostarcza zestaw fiszek do <code>QuizManager</code>a
+ * @author Platon
+ * @see QuizManager
+ *
+ */
+public class FlashcardSetProvider {
+
+}
---
+++ src/com/fishkey/QuizManager.java
@@ -0,0 +1,13 @@
+package com.fishkey;
+
+/**
+ * Zarzadza zestawem fiszek w quizie
+ * 
+ * udostepnia fiszki Sedziemu. Podejmuje decyzje, ktore fiszki maja byc
+ * w quizie, a z ktorych nie warto przepytywac.
+ * @author Platon
+ *
+ */
+public class QuizManager {
+	// TODO: wnetrze tej klasy
+}
