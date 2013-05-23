531cd2e259e17796acde281b7c6f2598a611401f
Platonn <platonn@platonn.com>
2013-05-23 12:39:04 +0200

    Wczytywanie fiszek przez FlashcardSetProvider
    ... a juz nie przez QuizState

--- src/com/fishkey/QuizState.java
+++ src/com/fishkey/QuizState.java
@@ -42,37 +42,10 @@ class QuizState {
 		countCorrectLastRounds	= 0;
 		roundNumber				= 1;
 		
-		this.importDataFromAssetsFile(context, "slowka.txt");
+		FlashcardSetProvider.importDataFromAssetsFile(context, "slowka.txt", flashcardSet);
 
 		currentSetSize=flashcardSetSize=flashcardSet.size();
 		flashcardSet.shuffle();									// Tasuj zestaw fiszek do przepytania
-	}
-	
-	/** 
-	 * wczytuje z pliku assets zestaw fiszek 
-	 * <p>
-	 * TODO: Robic to w tle - watku/usludze. A moze nawet wczytywac z inernetu badz lokalnej bazy danych.  
-	 * 
-	 * @param	context		obiekt klasy context - do obslugi plikow
-	 * @param	fileString	nazwa pliku do importu fiszek
-	 */
-	public void importDataFromAssetsFile(Context context, String fileName) {
-		try{
-			InputStream in = context.getAssets().open(fileName);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));	// Plik musi posiadac poprawne kodowanie UTF-8!
-			String line;																// Tu beda wczytywane kolejne linie
-			String[] text;																// Tablica bedzie przechowywac dwa stringi: odpowiedz oraz pytanie fiszki (w podanej kolejnosci)
-			do {
-				line = reader.readLine();												// Wczytanie nastepnej linii
-				if(line==null)															// Trzeba bylo tak zrobic, zeby mozna bylo uzyc potem funkcji split na nie-null'owym stringu
-					break;
-				text = line.split(" - ");												// Dwie spacje obok umozliwiaja podanie slowka zawierajacego "gêst¹ spacjê", np. semi-detached								// Rozdzielenie pytania od odpowiedzi przez separator (myslnik)
-				flashcardSet.add(new Flashcard(text[1].trim(),text[0].trim()));		// Wczytuje wg schematu: "PYTANIE - ODPOWIEDZ" (musza byc spacje wokol myslnika)
-			} while(true);
-			in.close();
-		} catch (IOException e) {
-			Log.i("Quiz", "QuizState - cannot open a file");
-		}
 	}
 	
 	/** 
