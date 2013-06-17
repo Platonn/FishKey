f52b27f41e71521de9a381c42789acc04257e1f7
Platonn <platonn@platonn.com>
2013-06-17 19:19:39 +0200

    Zalazek LearnAssistant'a, brzydki styl #20
    Wstepny mechanizm podejmowania decyzji przez LearnAssistanta. Podstawowo -
    jedynie jesli wspolczynnik jest >0 to nie pytaj z tej fiszki.
    
    Brzydki styl - brak komentarzy, duza zlozonosc.
    
    Dobrze by bylo przerobic FlashcardSet na LinkedHashMap!

--- src/com/fishkey/LearnAssistant.java
+++ src/com/fishkey/LearnAssistant.java
@@ -1,29 +1,78 @@
 package com.fishkey;
 
+import java.util.LinkedList;
+import java.util.Map;
+
 import android.content.Context;
 
 import com.fishkey.exceptions.QuizInitException;
+import com.fishkey.model.Flashcard;
 import com.fishkey.model.FlashcardSet;
+import com.fishkey.model.KnowledgeIndexSet;
 
 /**
- * odpowiada za efektywna nauke
+ * Asystent uczenia sie odpowiada za efektywna nauke.
  * <p>
- * podejmuje decyzje na podstawie statystyk - z ktorych fiszek warto, a z ktorych nie warto
+ * Podejmuje decyzje na podstawie statystyk - z ktorych fiszek warto, a z ktorych nie warto
  * przepytywac uzytkownika
  * @author Platon
  *
  */
 public class LearnAssistant {
-	StatisticsManager statsManager;
+	FlashcardSet flashcardSet;
+	FlashcardSet hangedFlashcardSet;
+	KnowledgeIndexSet knowledgeIndexSet;
 	
-	public LearnAssistant(){
-		statsManager = new StatisticsManager();
+	private static final int INCREASE_KI_VALUE = 1;
+	private static final int DECREASE_KI_VALUE = -3;
+	
+	Context context;
+	
+	public LearnAssistant(Context ctx) throws QuizInitException {
+		context = ctx;
+		flashcardSet 		= FlashcardSetProvider.getFlashcardSet(ctx);
+		knowledgeIndexSet		= KnowledgeIndexSetProvider.getKnowledgeIndexSet(ctx);
 	}
 	
-	public static FlashcardSet getFlashcardSet(Context context) throws QuizInitException {
-		FlashcardSet fs = FlashcardSetProvider.getFlashcardSet(context);
+	public FlashcardSet getFlashcardSet() {
 		//TODO: tu przejrzec zestaw fiszek i podjac decyzje, ktore z nich wstrzymac.
 		// zmodyfikowac takze statystyki dotyczace wstrzymanych fiszek
-		return fs;
+		hangedFlashcardSet = new FlashcardSet();
+		for (Map.Entry<Long, KnowledgeIndexSet.KnowledgeIndex> entry : knowledgeIndexSet.entrySet()) {
+			Long id = entry.getKey();
+			KnowledgeIndexSet.KnowledgeIndex ki = entry.getValue();
+			int value = ki.getValue();
+			if (value>0) { // TEMP: - nie robic tego tak - jesli value >0 , to usun z zestawu fiszke
+				for (Flashcard f : flashcardSet) {
+					if (f.getId()==id)
+						hangedFlashcardSet.add(f);
+				}
+			}
+			flashcardSet.removeAll(hangedFlashcardSet);
+		}
+		return flashcardSet;
+	}
+	
+	/* TODO: zrobić to dla listy raportow z konca rund */
+	public void updateKnowledgeIndex(LinkedList<QuizRound> roundsList) {
+		for(QuizRound round : roundsList){
+			FlashcardSet fsCorrect = round.getAnsweredCorrectSet();
+			FlashcardSet fsWrong = round.getAnsweredWrongSet();
+			for (Flashcard f : fsCorrect){
+				Long id = f.getId();
+				KnowledgeIndexSet.KnowledgeIndex ki = knowledgeIndexSet.get(id);
+				int kiValue = ki.getValue();
+				ki.setValue(kiValue+INCREASE_KI_VALUE);
+				ki.setCurrentDate();
+			}
+			for (Flashcard f : fsWrong){
+				Long id = f.getId();
+				KnowledgeIndexSet.KnowledgeIndex ki = knowledgeIndexSet.get(id);
+				int kiValue = ki.getValue();
+				ki.setValue(kiValue+DECREASE_KI_VALUE);
+				ki.setCurrentDate();
+			}
+		}
+		KnowledgeIndexSetProvider.archiveKnowledgeIndexSet(context, knowledgeIndexSet);
 	}
 }
