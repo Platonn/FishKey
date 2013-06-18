8a1a65ae2ebfccb163ccd72b12f4210d01119366
Platonn <platonn@platonn.com>
2013-06-18 18:10:47 +0200

    Dodano klase ShuffleLinkedList
    Dziedziczy ona z LinkedList i posiada metode mieszajaca jej elementy.

---
+++ src/com/fishkey/model/ShuffleLinkedList.java
@@ -0,0 +1,26 @@
+package com.fishkey.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class ShuffleLinkedList<T> extends LinkedList<T> {
+	private static final long serialVersionUID = -7351042828068144420L;
+
+	/**
+	 * generator losowy
+	 */
+	private static Random random = new Random();
+	
+	/**
+	 * ustawia elementy listy w losowej kolejnosci
+	 */
+	public void shuffle() {
+		int n = this.size();
+	    random.nextInt();
+	    for (int i = 0; i < n; i++) {
+	    	int change = i + random.nextInt(n - i);
+	    	Collections.swap(this, i, change);
+	    }
+	}
+}
