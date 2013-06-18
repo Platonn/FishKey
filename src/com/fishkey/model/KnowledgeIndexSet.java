baa9cf6aaea6630a206b06cea5feea8f4631463e
Platonn <platonn@platonn.com>
2013-06-18 19:21:04 +0200

    Usuniecie ciala klasy KnIdx z KnowledgeIndexSet
    Dodanie metody toString() i nadanie KnowledgeIndexSet pola id -
    id zestawu fiszek, ktorego dotycze ten KISet

--- src/com/fishkey/model/KnowledgeIndexSet.java
+++ src/com/fishkey/model/KnowledgeIndexSet.java
@@ -1,8 +1,5 @@
 package com.fishkey.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.LinkedHashMap;
 
 /**
@@ -10,53 +7,18 @@ import java.util.LinkedHashMap;
  * @author Platon
  *
  */
-public class KnowledgeIndexSet extends LinkedHashMap<Long, KnowledgeIndexSet.KnowledgeIndex> {
+public class KnowledgeIndexSet extends LinkedHashMap<Long, KnowledgeIndex> {
+	private static final long serialVersionUID = 3726907896583249801L;
 	
-	/** Format daty zapisanej jako tekst */
-	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	/** id zestawu fiszek, ktorego dotyczy ten Set ze wspolczynnikami znajomosci fiszek */
+	Long id;
 	
-	/**
-	 * wspolczynnik znajomosci fiszki wraz z data ostatniego przepytania z tej fiszki
-	 * @author Platon
-	 *
-	 */
-	public class KnowledgeIndex {
-		
-		/** wartosc wspolczynnika znajomosci fiszki */
-		private int value;
-		
-		/** data ostatniego przepytania z tej fiszki */
-		private Date date;
-		
-		/**
-		 * @param v			wartosc wspolczynnika znajomosci fiszki
-		 * @param dateText	data ostatniego przepytania z fiszki
-		 * @throws ParseException gdy data zostala podana w formacie innym niz
-		 * 							<code>KnowledgeIndexSet#dateFormat</code>
-		 * @see KnowledgeIndexSet#dateFormat
-		 */
-		public KnowledgeIndex(int v, String dateText) throws ParseException {
-			value	= v;
-			date	= dateFormat.parse(dateText);
-		}
-		
-		/** setter wspolczynnika znajomosci slowka */
-		public void setValue(int v){
-			value = v;
-		}
-		
-		/** ustawia date ostatniego przepytania slowka na biezaca */
-		public void setCurrentDate() {
-			date = new Date();
-		}
-		
-		/** getter wspolczynnika znajomosci slowka */
-		public int getValue() {
-			return value;
-		}
-		
-		public String getDateAsString() {
-			return dateFormat.format(date);
-		}
+	public KnowledgeIndexSet(Long id) {
+		this.id = id;
+	}
+	
+	/** getter id zestawu fiszek, ktorego dotyczy ten Set ze wspolczynnikami znajomosci fiszek */
+	public Long getId() {
+		return id;
 	}
 }
