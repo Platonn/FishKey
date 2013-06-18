a40bae31b401696492805f84e69cf3e31236b5cb
Platonn <platonn@platonn.com>
2013-06-18 18:49:24 +0200

    FlashcardSet ext. LinkedHashMap. +FlashcardIdList
    Wyrzucono wiekszosc metod FlashcardSet. Teraz ta klasa trzyma w postaci
    HashMap'y fiszki indeksowane ich unikalnym id.
    
    QuizState ma w przyszlosci trzymac jeden FlashcardSet.
    Kazda runda moze trzymac juz tylko liste id fiszek do przepytania -
    np. w FlashcardIdList (dziedziczacej po ShuffleLinkedList<Long>).
    	new file:   src/com/fishkey/model/FlashcardIdList.java
    	modified:   src/com/fishkey/model/FlashcardSet.java

---
+++ src/com/fishkey/model/FlashcardIdList.java
@@ -0,0 +1,13 @@
+package com.fishkey.model;
+
+/** 
+ * Lista id fiszek.
+ * <p>
+ * Mozna te liste tasowac.
+ * @author Platon
+ *
+ */
+public class FlashcardIdList extends ShuffleLinkedList<Long> {
+	private static final long serialVersionUID = 2638939129068800264L;
+	
+}
--- src/com/fishkey/model/FlashcardSet.java
+++ src/com/fishkey/model/FlashcardSet.java
@@ -1,130 +1,47 @@
 package com.fishkey.model;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.LinkedHashMap;
 
 /**
- * kolekcja fiszek
+ * zestaw fiszek o unikalnym id
  * @author Platon
  *
  */
-public class FlashcardSet {
-	/**
-	 * lista z fiszkami
-	 */
-	private LinkedList<Flashcard> list;
+public class FlashcardSet extends LinkedHashMap<Long,Flashcard>{
+	private static final long serialVersionUID = -5077990913112152508L;
 	
 	/**
-	 * rozmiar listy fiszek
+	 * id zestawu fiszek
 	 */
-	private int size;
+	private Long id;
 	
 	/**
-	 * generator losowy
-	 * <p>
-	 * sluzy do tasowania zestawu fiszek za kazdym razem inaczej
+	 * nazwa zestawu fiszek
 	 */
-	private static Random random = new Random();
-	
-	/**
-	 * nazwa zestawu fiszek, domyslnie null
-	 */
-	private String name = null; 
-	
+	private String name; 
+		
 	/**
 	 * inicjalizuje liste fiszek
 	 */
-	public FlashcardSet(){
-		list = new LinkedList<Flashcard>();
-		size = 0;
-	}
-
-	/**
-	 * wyjmuje i zwraca aktualnie pierwsza fiszke z zestawu
-	 * 
-	 * @return	Flashcard	ostatnia fiszka na liscie,
-	 */
-	public Flashcard pop() {
-		if(!isEmpty()) --size;
-		return list.poll();
+	public FlashcardSet(Long i, String n){
+		super();
+		id		= i;
+		name	= n;
 	}
 	
 	/**
-	 * wstawia fiszke na koniec
-	 * 
-	 * @param	f	fiszka do wstawienia do listy
+	 * zwraca id zestawu fiszek
+	 * @return	id zestawu fiszek
 	 */
-	public void add(Flashcard f) {
-		++size;
-		list.offer(f);
+	public Long getId() {
+		return id;
 	}
 	
 	/**
-	 * wstawia fiszke na koniec listy fiszek
-	 * 
-	 * @return	rozmiar listy fiszek
-	 */
-	public int size() {
-		return size;
-	}
-	
-	/**
-	 * zwraca info, czy zestaw jest pusty
-	 * 
-	 * @return	true, jesli zestaw jest pusty, false w przeciwnym przypadku
-	 */
-	public boolean isEmpty() {
-		return size==0;
-	}
-	
-	/**
-	 * oproznia liste fiszek
-	 */
-	public void clear() {
-		list.clear();
-		size=0;
-	}
-	
-	/**
-	 * tasuje zestaw fiszek
-	 * <p>
-	 * ustawia fiszki w losowej kolejnosci
-	 */
-	public void shuffle() {
-		int n = list.size();
-	    random.nextInt();
-	    for (int i = 0; i < n; i++) {
-	    	int change = i + random.nextInt(n - i);
-	    	Collections.swap(list, i, change);
-	    }
-	}
-	
-	/**
-	 * kopiuje do zestawu wszystkie fiszki z zestawu podanego jako argument 
-	 * @param fs	zestaw fiszek do skopiowania
-	 */
-	public void addAllFrom(FlashcardSet fs) {
-		list.addAll(fs.list);
-		size += fs.size();
-	}
-	
-	/**
-	 * zwraca nazwe zestawu fiszek badz null, jesli brak nazwy quizu.
-	 * <p>
-	 * Nazwe zestawu fiszek mozna ustawic za pomoca metody <code>setName(String)</code>
-	 * @return	nazwa quizu badz null, jesli brak nazwy quizu
-	 * @see #setName(String)
+	 * zwraca nazwe zestawu fiszek
+	 * @return	nazwa zestawu fiszek
 	 */
 	public String getName() {
 		return name;
-	}
-	
-	/**
-	 * ustawia nazwe dla zestawu fiszek
-	 * @param n	nazwa zestawu fiszek
-	 */
-	public void setName(String name){
-		this.name = name;
 	}
 }
